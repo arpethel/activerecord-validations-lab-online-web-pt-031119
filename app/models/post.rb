7e8ec1ee6d34d47237eb07e294c0057dd64cb331
Ryan Pethel <arpethel@gmail.com>
2019-08-01 22:56:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} not a valid category. Category must either be Fiction or Non-Fiction."}
   validate :title_must_be_clickbait
- 
+
   def title_must_be_clickbait
     if title.present? && !title.include?("True", "Facts")
       errors.add(:title, "must be clickbait")
