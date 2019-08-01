c5f1a8ee7cd29300fb09496d3cba980957b28e05
Ryan Pethel <arpethel@gmail.com>
2019-08-01 22:53:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,5 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 10 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} not a valid category. Category must either be Fiction or Non-Fiction."}
-  validates :title, inclusion: { in: %w(True Facts),
-    message: "%{value} not a valid Title. Title must be clickbait."}
+  validates :not_clickbait, acceptance: true, message: "clickbait only!"
 end
