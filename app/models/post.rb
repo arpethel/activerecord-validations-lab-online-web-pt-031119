b036a24eb96ee069f5a6709fffdb086c9e1f3861
Ryan Pethel <arpethel@gmail.com>
2019-08-01 22:40:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 10 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} not a valid category. Category must either be Fiction or Non-Fiction."}
 end
